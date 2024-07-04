8dafdc2b058d14fd9c0c9ee2625064697f0229ca
SquareScreamYT <kohkaifeng06@gmail.com>
2024-07-04 18:01:36 +0800

    Update

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
   var coll = document.getElementsByClassName("collapsible");
   for (var i = 0; i < coll.length; i++) {
-      coll[i].addEventListener("click", function() {
-          this.classList.toggle("active");
-          var content = this.nextElementSibling;
-          if (content.style.maxHeight) {
-              content.style.maxHeight = null;
-          } else {
-              content.style.maxHeight = content.scrollHeight + "px";
-          }
-      });
+  coll[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var content = this.nextElementSibling;
+  if (content.style.maxHeight) {
+    content.style.maxHeight = null;
+  } else {
+    content.style.maxHeight = content.scrollHeight + "px";
   }
-});
+});}});
