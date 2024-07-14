f9dbf67db6af1f5975103dbabe2dba46c8c522b9
SquareScreamYT <kohkaifeng06@gmail.com>
2024-07-14 20:03:50 +0800

    Viossa Dictionary

---
+++ viossa-dictionary/script.js
@@ -0,0 +1,35 @@
+function changeColorScheme() {
+  var selectedScheme = document.getElementById('color-scheme').value;
+  document.body.className = selectedScheme + '-scheme';
+  localStorage.setItem('colorScheme', selectedScheme);
+}
+
+function loadColorScheme() {
+  var savedScheme = localStorage.getItem('colorScheme');
+  if (savedScheme) {
+    document.body.className = savedScheme + '-scheme';
+    document.getElementById('color-scheme').value = savedScheme;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', loadColorScheme);
+
+fetch('words.json')
+  .then(response => response.json())
+  .then(data => {
+    const wordsContainer = document.getElementById('sidebar');
+    const definitionsElement = document.getElementById('wordDetails');
+    
+    data.forEach((item, index) => {
+      const button = document.createElement('button');
+      button.textContent = item.word;
+      button.addEventListener('click', () => {
+        definitionsElement.innerHTML = `
+          <p>Definition 1: ${item.word.definition1}</p>
+          <p>Definition 2: ${item.word.definition2}</p>
+        `;
+      });
+      wordsContainer.appendChild(button);
+    });
+  })
+  .catch(error => console.error('Error:', error));
