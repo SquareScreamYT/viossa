f5a83202c7e624e52ddec4e94c6068a6e4eb9a76
SquareScreamYT <kohkaifeng06@gmail.com>
2024-08-06 16:49:16 +0800

    test 2

---
+++ kotobalibre/alkotoba.js
@@ -0,0 +1,35 @@
+fetch('words.json')
+  .then(response => response.json())
+  .then(data => {
+    var wordTable = document.getElementById('wordTable');
+    var contents = `
+      <thead>
+        <tr>
+          <th>kotoba</th>
+          <th>klani</th>
+          <th>imi</th>
+          <th>riso 1</th>
+          <th>riso 2</th>
+        </tr>
+      </thead>
+      <tbody>
+    `;
+
+    data.forEach(wordObject => {
+      const wordKey = Object.keys(wordObject)[0];
+      contents += `
+        <tr>
+          <td>${wordKey}</td>
+          <td>${wordObject[wordKey].category.join(", ")}</td>
+          <td>${wordObject[wordKey].definition}</td>
+          <td>${wordObject[wordKey].image ? `<img src="${wordObject[wordKey].image}" height="50" width="auto">` : ''}</td>
+          <td>${wordObject[wordKey].image2 ? `<img src="${wordObject[wordKey].image2}" height="50" width="auto">` : ''}</td>
+        </tr>
+      `;
+    });
+
+    console.log(contents);
+    contents += '</tbody>';
+    wordTable.innerHTML = contents;
+  })
+  .catch(error => console.error('Error: ', error));
